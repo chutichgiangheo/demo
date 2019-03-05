c8b193830e2f167579bf33979ca32b424ac65fb1
chutichgiangheo <nguyenvantruong454@gmail.com>
2019-03-05 11:07:36 +0700

    Change

--- test/Form1.cs
+++ test/Form1.cs
@@ -22,5 +22,11 @@ namespace test
             int so2 = 8;
             int so3 = so1 + so2;
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            int so1 = 4;
+            int so2 = 8;
+        }
     }
 }
