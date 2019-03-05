43716964918dc748094196f60e89dc46a2c3aa51
chutichgiangheo <nguyenvantruong454@gmail.com>
2019-03-05 11:04:14 +0700

    Change Code

--- test/Form1.cs
+++ test/Form1.cs
@@ -15,5 +15,12 @@ namespace test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int so1 = 4;
+            int so2 = 8;
+            int so3 = so1 + so2;
+        }
     }
 }
